5fed5998257816b85debe8880ed274d7ca683c7f
Joe,Jou <pk0204dg888@gmail.com>
2025-04-20 11:36:42 +1000

    fix: joy

--- app/client/src/App.js
+++ app/client/src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
       <h1>Hello React from Docker</h1>
+      <h1>Joy Joe Balloon Twisting</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
